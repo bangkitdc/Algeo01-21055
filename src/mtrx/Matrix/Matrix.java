290cb73432b09b61a7aaa561c1620f45bd73df7f
bangkitdc@gmail.com <bangkitdc@gmail.com>
2022-09-21 02:08:10 +0700

    determinan + structure

--- src/mtrx/Matrix/Matrix.java
+++ src/mtrx/Matrix/Matrix.java
@@ -1,36 +1,32 @@
 package mtrx.Matrix;
 
 public class Matrix{
-
     public static final int ROW_CAP = 100;
     public static final int COL_CAP = 100;
+    private double[][] mem;
+    private int ROW_EFF;
+    private int COL_EFF;
 
-    public static class MatrixType {
-        private double[][] mem;
-        private int ROW_EFF;
-        private int COL_EFF;
+    public Matrix (int row, int col) {
+        this.ROW_EFF = row;
+        this.COL_EFF = col;
+        this.mem = new double [row][col];
+    } 
 
-        public MatrixType (int row, int col) {
-            this.ROW_EFF = row;
-            this.COL_EFF = col;
-            this.mem = new double [row][col];
-        } 
+    // SELEKTOR 
+    public double getELMT(int row, int col) {
+        return this.mem[row][col];
+    }
 
-        // SELEKTOR 
-        public double GetELMT(int row, int col) {
-            return this.mem[row][col];
-        }
+    public void setELMT(int row, int col, double d) {
+        this.mem[row][col] = d;
+    }
 
-        public void SetELMT(int row, int col, float value) {
-            this.mem[row][col] = value;
-        }
+    public int getRow() {
+        return ROW_EFF;
+    }
 
-        public int GetRowEff() {
-            return ROW_EFF;
-        }
-
-        public int GetColEff() {
-            return COL_EFF;
-        }
+    public int getCol() {
+        return COL_EFF;
     }
 }
