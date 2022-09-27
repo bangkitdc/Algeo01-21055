9399a6fcca3d82b86bdc0ee2b235c7e5b78f746a
bagas003 <103223351+bagas003@users.noreply.github.com>
2022-09-28 01:28:27 +0700

    tambah swapCount
    tp keknya salah deh konsepnya, tlg cek pls

--- src/mtrx/Methods/GaussJordan.java
+++ src/mtrx/Methods/GaussJordan.java
@@ -12,16 +12,6 @@ public class GaussJordan {
 		return m.getCol();
 	}
 	
-	public static void sortMatrix(Matrix m) {
-
-		for (int i=0; i<m.getRow()-1; i++) {
-			for (int j=i+1; j<m.getRow(); j++) {
-				if (firstNotZero(m, j) < firstNotZero(m, i)) {
-					m.swapRow( i, j);
-				}
-			}
-		}
-	}
 
 	public static void makeOne(Matrix m) {
 		for (int i=0; i<m.getRow(); i++) {
@@ -56,9 +46,23 @@ public class GaussJordan {
 		}
 	}
 	
+	public static int swapCount;
+	
+	public static void sortMatrix(Matrix m) {
+		for (int i=0; i<m.getRow()-1; i++) {
+			for (int j=i+1; j<m.getRow(); j++) {
+				if (firstNotZero(m, j) < firstNotZero(m, i)) {
+					m.swapRow( i, j);
+					swapCount++;
+				}
+			}
+		}
+	}
+	
 	public static void determinantOBE(Matrix m) {
 		// I.S. Matrix
 		// F.S. Matrix di OBE tp diagonal gk dibuat 1
+		swapCount = 0;
 		sortMatrix(m);
 		for (int i=0; i<m.getRow()-1; i++) {
 			OBE(m,i);
